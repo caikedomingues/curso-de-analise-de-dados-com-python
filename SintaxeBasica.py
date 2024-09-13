8aa4ebbac4f5294941f423dc7ad726c4f8a93e39
Caike De Oliveira Domingues <caike.dom@gmail.com>
2024-09-13 16:34:16 -0300

    primeira aula do curso
    sintaxe básica do python

---
+++ SintaxeBasica.py
@@ -0,0 +1,72 @@
+
+
+# O que é python?
+
+# Python é uma linguagem de programação popular. Foi craida por Guido
+# Van Rossum e foi lançada em 1991.
+
+#Python pode ser usado para:
+# 1 - Inteligência Artificial
+# 2 - Machine Learning
+# 3 - Desenvolvimento de software
+# 4 - Desenvolvimento de aplicativos
+# 5 - Automações(RPA)
+# 6 - IOT(Internet das Coisas)
+# 7 - Muitas outras
+
+
+# Sintaxe Básica
+# Podemos utilizar ''(aspas simples)
+# Podemos utilizar ""(aspas duplas)
+# utilizando o \n para pular uma linha na impressão do valor
+# usamos a hashtag para comentar algo
+
+
+#Comando print(Serve para imprimir conteudos na tela)
+print("Bem-vindo ao curso de python")
+
+#Também é possivel comentar usando 3 aspas simples
+
+#Exemplo de comentario com 6 aspas simples
+
+'''
+Comentários com aspas simples
+
+'''
+
+print("Olá mundo \n pulou uma linha")
+
+
+# Operadores Matemáticos
+# 1 - Soma
+# 2 - subtração
+# 3 - Multiplicação
+# 4 - Divisão
+# 5 - Exponenciação
+# 6 - Resto da Divisão
+# 7 - Divisão do chão
+
+#soma
+print(1 + 3 + 4)
+
+#subtração
+
+print(1 - 1)
+
+# multiplicação
+print(2 * 2)
+
+# Divisão
+
+print(4 / 2)
+
+# Exponenciação
+print(2 ** 2)
+
+#Resto da divisão
+
+print(3 % 4)
+
+#Divisão de chão 
+
+print(3//4)
