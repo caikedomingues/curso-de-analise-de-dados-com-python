afbf770c884ed8789dd18949b4b6c0b667938511
Caike De Oliveira Domingues <caike.dom@gmail.com>
2024-09-18 16:39:34 -0300

    conetudo da aula de operadores de associação

--- OperadoresAssociação.py
+++ OperadoresAssociação.py
@@ -8,3 +8,19 @@
 # 2. not in: Retorna True se uma sequencia com o valor especificado 
 # não estiver presente no objeto
 
+
+# Para demonstrar o uso do operador vamos criar uma lista
+
+lista_açoes = ['Ragazzo', 'via', 'carrefour']
+
+# vamos verificar se 'via' existe na lista
+
+print("Tem via na lista? ", 'via' in lista_açoes)
+
+print("Não tem via na lista? ", 'via' not in lista_açoes)
+
+# Agora vamos testar os operadores com um dado que não existe na lista
+
+print("Tem 22 na lista? ", '22' in lista_açoes)
+
+print("Nao tem 22 na lista? ", '22' not in lista_açoes)
