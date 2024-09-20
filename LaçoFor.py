e47828ca8188a006a52ce2ef83eeb16d7a59824c
Caike De Oliveira Domingues <caike.dom@gmail.com>
2024-09-20 15:26:38 -0300

    Laço for

---
+++ LaçoFor.py
@@ -0,0 +1,13 @@
+
+# Estrutura do loop For
+
+# O loop for do python é usado para travessia sequencial, ou seja,
+# é usado para iterar sobre um iteravel como atrings,tuplas, listas,
+# etc. Ele se enquadra na categoria de iteração definida. Iterações
+# definidas significam que o numero de repetições é especificado
+# explicitamente com antecedência.
+
+# Estrutura do for:
+# for variável in objeto:
+#       Loop
+
