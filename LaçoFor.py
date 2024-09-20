df5ac03d37701bef0fcdf097d4c8ab1994faf223
Caike De Oliveira Domingues <caike.dom@gmail.com>
2024-09-20 15:42:54 -0300

    Update LaçoFor.py

--- LaçoFor.py
+++ LaçoFor.py
@@ -11,3 +11,21 @@
 # for variável in objeto:
 #       Loop
 
+# No primeiro exemplo, vamos imprimir 10 valores usando do (0 ao 9)
+# Como vimos anteriormente a estrutura do for é composta por 2 fatores,
+# o primeiro é o nome da variável e o segundo é o objeto que será percorrido / iterado pelo for.
+ 
+for qualquerCoisa in range(10):
+    
+    print(qualquerCoisa)
+    
+# Esse exemplo ira somar os valores com 2
+for outracoisaqualquer in range(10):
+    
+    print("com mais 2: ",outracoisaqualquer + 2)
+    
+
+# Esse exemplo ira elevar os valores ao quadrado
+for quadrado in range(10):
+    
+    print("Elevado ao quadrado: ", quadrado **2)
