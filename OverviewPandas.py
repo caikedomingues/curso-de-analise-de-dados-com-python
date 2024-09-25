0b18772ed9b5ac408d45c3e9b6aea227fe042808
Caike De Oliveira Domingues <caike.dom@gmail.com>
2024-09-25 15:49:47 -0300

    ultima parte dos exercicios de pandas

--- OverviewPandas.py
+++ OverviewPandas.py
@@ -9,9 +9,6 @@
 # para acessarmos o arquivo csv e tratar os dados.
 import pandas as pd
 
-# import da biblioteca np
-import numpy as np
-
 # Leitura do arquivo: é necessário atribuir a uma variável, o comando de 
 # acesso ao arquivo csv para podermos acessar os metodos necessários para
 # a análise de dados.
@@ -120,3 +117,86 @@ print(df.value_counts())
 
 print("FREQUENCIA DOS VALORES DA COLUNA GENDER")
 print(df.gender.value_counts())
+
+# Agora vamos criar uma lista que conterá as colunas de pontuações
+# das matérias
+# lista provas
+
+provas = ['math score', 'reading score', 'writing score']
+
+# Agora podemos organizar / ordenar o dataset de acordo com a nossa
+# lista
+
+# Organzação do dataset de acordo com a lista de provas
+
+# ira ordernar o dataframe com base nas colunas da lista provas
+# sort_values: Função do pandas que ordena os dados com base
+# nas colunas
+# by: recebe como parametro as colunas que serão usadas na 
+# ordenação do dataset
+# ascending: Tem como obejtivo especificar se a ordenação
+# deve ser crescente ou descrescente. Se fosse True, a ordenação
+# seria crescente
+# reset index: após ordenar os dados, os indices originais(posições das linhas) podem ficar desordenados, então, o intuito da função é reiniciar
+#  o indice para começar de 0, 1, 2
+
+
+# Como no vscode não há espaço suficiente, temos que usar 2 funções
+# do pandas para garantir que todas as informações sejam mostradas
+# corretamente
+
+#pd.set_option('display.max_columns', None) (Ira mostrar todas as colunas)
+
+#pd.set_option('display.max_rows', None)  (Ira mostrar todas as colunas)
+
+# Se quiser voltar aos padrões basta digitar
+# os comando: pd.set_option('display.max_columns'), pd.set_option('display.max_rows') 
+print("ORGANIZAÇÃO DAS COLUNAS DE PONTUAÇÃO DAS MATÉRIAS")
+
+print(df.sort_values(by = provas, ascending = False).reset_index(drop=True))
+
+# Coluna com a média das provas 
+# Irá calcular a média de cada coluna da tabela
+print("MÉDIA DAS PROVAS")
+# Esse trecho ira criar uma nova coluna
+media = df['mean'] = df[provas].mean(axis=1)
+# Ira mostrar todas as colunas
+pd.set_option('display.max_columns', None)
+print(media)
+
+# Vamos verificar se a coluna foi criada.
+print("AS 5 PRIMEIRA LINHAS INCLUINDO A NOVA COLUNA")
+print(df.head())
+
+# Outra funcionalidade do pandas é possibilitar consultas e filtros
+# das informações do dataframe
+
+# Por exemplo, vamos filtrar os alunos que: são do sexo masculino, não
+# fizeram o curso de preparação para testes e possuem notas maior ou 
+# igual a 70.
+
+# Observação: casos as colunas estejam com nomes separados, devemos colocar o nome entre crases para funcionar.
+print("FILTROS")
+print(df.query('(gender == "male") & (`test preparation course` == "none") & (`math score`>= 70)'))
+
+
+
+# O método loc[] é usado para selecionar linhas ou colunas de um DataFrame com base em rótulos (ou seja, condições booleanas, índices ou nomes de colunas).
+print("FILTRO COM A FUNÇÃO LOC")
+
+print(df.loc[(df.gender == 'male') & (df['test preparation course'] == 'none') & (df['math score'] >= 70)])
+
+# Agrupamentos: agrupa os dados por generos e obtem estastisticas
+# descritivas
+
+# Groupby: é usado para agrupar os dados do dataframe com base 
+# nos valores de uma coluna
+
+# provas é a lista de colunas que contém as pontuações das provas
+
+# agg(['medan','median']): agg é o metodo que você usa para agregar
+# dados, ele permite aplicar funções de agregação(como o calculo de médias, por exemplo).
+
+print("AGRUPAMENTOS POR GENÊROS")
+print(df.groupby(by='gender')[provas].agg(['mean', 'median']))
+
