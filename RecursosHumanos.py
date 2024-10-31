e5fba9fc4b23e84ec88123e0491c2a8b3f9e63be
Caike De Oliveira Domingues <caike.dom@gmail.com>
2024-10-31 12:29:13 -0300

    inicio do projeto recursos humanos

---
+++ RecursosHumanos.py
@@ -0,0 +1,16 @@
+
+# Neste projeto vamos estudar sobre: Tempo de Experiência vs Salário 
+
+# Import das bibliotecas necessárias
+
+# Import da biblioteca que ira possibilitar acessar o dataset,
+# manipular os dados, analisar os dados, etc
+import pandas as pd
+
+# Import da biblioteca que ira possibilitar criar e manipular
+# gráficos
+
+import matplotlib.pyplot as plt
+
+# Import da bibilioteca que contém uma grande variedade de 
+# gráficos
