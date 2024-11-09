c5e5be1ab5d4c568d61396f2ff151167b8d33786
Caike De Oliveira Domingues <caike.dom@gmail.com>
2024-11-09 16:57:04 -0300

    inicio do exercicio per capita

---
+++ RendaCapita.py
@@ -0,0 +1,7 @@
+# Conjunto de dados Econômicos do Brasil 
+
+# Dados da renda per capita dos estados união
+
+# Renda per Capita: É um dos indicadores socioeconomicos que avaliam
+# o grau de desenvolvimento econômico de um determinado lugar. A
+# média é obtida através da divisão do Produto Nacional Bruto
